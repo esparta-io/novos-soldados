36eb888edc6b998415912edad47e89c5cbdf61e2
murilo0121 <murilo0121@gmail.com>
2016-08-21 21:07:44 -0300

    MVC, assets, singleton e model

---
+++ ToDo/ToDo/Singleton.swift
@@ -0,0 +1,52 @@
+//
+//  Singleton.swift
+//  Teste
+//
+//  Created by Murilo Erhardt on 21/08/16.
+//  Copyright © 2016 Murilo Erhardt. All rights reserved.
+//
+
+import UIKit
+
+class Singleton{
+    
+    var listTodo: [TaskModel] = []
+    var listDone: [TaskModel] = []
+    
+    
+    class var sharedInstance: Singleton{
+        struct Static {
+            static let instance: Singleton = Singleton()
+        }
+        return Static.instance
+    }
+    
+    func getListTodoSize() -> Int{
+        return listTodo.count
+    }
+    
+    func getListDoneSize() -> Int{
+        return listDone.count
+    }
+    
+    func getBackGroundCollor() -> UIColor{
+        return UIColor(red: 31/255, green: 27/255, blue: 27/255, alpha: 1.0)
+    }
+    
+    func getBackGroundCollorButton() -> UIColor{
+        return UIColor(red: 54/255, green: 48/255, blue: 48/255, alpha: 0.9)
+    }
+    
+    func insertNewTaskOnList(task: TaskModel){
+        if(task.isFinish == false){
+            listTodo.append(task)
+        }
+        if(task.isFinish == true){
+            listDone.append(task)
+        }
+        else{
+            print("erro")
+        }
+    }
+    
+}
